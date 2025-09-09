b6c58e0d04766c4bf42e05bcb303c2820ade5294
Paulodemczuk <paulo@MacBook-Air-de-Paulo.local>
2025-09-09 15:26:11 -0300

    Criando classe 'mãe' para cadastros

---
+++ gerenciamento/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        Pessoa teste = new Pessoa("Marcos",16,"telefone");
+        System.out.println(teste.getNome());
+    }
+}
---
+++ gerenciamento/src/Pessoa.java
@@ -0,0 +1,34 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String telefone;
+
+    public Pessoa(){
+        this.nome = "";
+        this.idade = 0;
+        this.telefone = "";
+    }
+    public Pessoa(String nome, int idade, String telefone){
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    };
+    public void setIdade(int idade) {
+        this.idade = idade;
+    };
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    };
+    public String getNome(){
+        return this.nome;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+    public String getTelefone(){
+        return this.telefone;
+    }
+}
