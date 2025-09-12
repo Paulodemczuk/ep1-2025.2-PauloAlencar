dc86478bfe35caf39be87bafbbe258f6aab21b49
Paulodemczuk <paulodemczuk@gmail.com>
2025-09-12 12:58:25 -0300

    Nova classe Paciente

---
+++ Application/src/Entidades/Paciente.java
@@ -0,0 +1,26 @@
+package Entidades;
+
+public class Paciente extends Pessoa {
+    public String cpf;
+    
+    public Paciente(){
+        super();
+        this.cpf = "";
+        
+    }
+    public Paciente(String nome, int idade, String telefone, String cpf){
+        super(nome, idade, telefone);
+        this.cpf = cpf;
+    }
+    
+    
+    @Override
+    public void printInformacoes(){
+        System.out.println("----------------------");
+        System.out.println("nome:"+ this.getNome());
+        System.out.println("idade:"+ this.getIdade());
+        System.out.println("telefone:"+ this.getTelefone());
+        System.out.println("CPF: "+ this.cpf);
+        System.out.println("----------------------");
+    }
+}
