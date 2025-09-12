dd57747c5b2c0249d9dfba67a36ef6d277f5fc1c
Paulodemczuk <paulodemczuk@gmail.com>
2025-09-12 12:55:59 -0300

    Criado Prototipo de menu

--- Application/src/App.java
+++ Application/src/App.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Pessoa teste = new Pessoa("Marcos",16,"telefone");
-        System.out.println(teste.getNome());
+        System.out.println("-----Menu principal------");
+        System.out.println("1. Menu Paciente");
+        System.out.println("2. Menu Medico");
+        System.out.println("3. Menu Relatórios");
+        System.out.println("4. Menu Cadastramento");
+        Scanner sc = new Scanner(System.in);
+        int menu = sc.nextInt();
+        sc.close();
+        System.out.println("O menu escolhido foi: "+ menu);
     }
 }
