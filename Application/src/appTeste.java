32bbaee75d635f87f04b8712c4b7abdaf8d56454
Paulodemczuk <paulodemczuk@gmail.com>
2025-09-13 20:21:58 -0300

    arquivo para testes

---
+++ Application/src/appTeste.java
@@ -0,0 +1,15 @@
+import Entidades.*;
+public class appTeste {
+    public static void main(String[] args) {
+        Paciente machucado = new Paciente();
+        machucado.setNome("Super Tadinho");
+        machucado.setIdade(70);
+        machucado.setCpf("123.321.892-20");
+        machucado.printInformacoes();
+        Medico med1 = new Medico();
+        med1.setNome("Doutor forte");
+        med1.setCrm("21345 SP");
+        med1.setEspecialidade("Cardiologista");
+        med1.printInformacoes();
+    }
+}
