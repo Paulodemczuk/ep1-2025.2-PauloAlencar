3224ead96a364c9260a45a6c785b71b12b70ccb1
Paulodemczuk <paulodemczuk@gmail.com>
2025-09-13 20:21:40 -0300

    Nova classe agenda

---
+++ Application/src/Entidades/Agenda.java
@@ -0,0 +1,11 @@
+package Entidades;
+
+import java.util.ArrayList;
+
+public class Agenda {
+    private ArrayList<ArrayList<String>> agenda;
+
+    public Agenda(){
+        this.agenda = new ArrayList<>(); 
+    }
+}
