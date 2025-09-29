4a510102a6dfcaf127b4bdc966e5671a3273e833
Paulodemczuk <paulodemczuk@gmail.com>
2025-09-29 17:09:43 -0300

    Metodos cadastrarPaciente e cpfCadastrado criados

--- Application/src/Servicos/PacienteServicos.java
+++ Application/src/Servicos/PacienteServicos.java
@@ -1,5 +1,35 @@
 package Servicos;
 
-public class PacienteServicos {
+import Entidades.Paciente;
+import java.util.ArrayList;
 
+public class PacienteServicos {
+    private ArrayList<Paciente> pacientes;
+
+    public PacienteServicos() {
+        this.pacientes = new ArrayList<>();
+    }
+
+    public boolean cadastrarPaciente(String nome, int idade, String telefone, String cpf){
+        boolean cadastrou = cpfCadastrado(cpf);
+        if(cadastrou){
+            Paciente novoPaciente = new Paciente(nome, idade, telefone, cpf);
+            pacientes.add(novoPaciente);
+            cadastrou = true;
+        }
+        return cadastrou;
+    }
+
+    public boolean cpfCadastrado(String cpf){
+        boolean existe = false;
+        for(Paciente paciente : pacientes){
+            if(paciente.getCpf().equals(cpf)){
+                existe = true;
+                break;
+            }
+        
+        }
+        return existe;
+    }
+    
 }
