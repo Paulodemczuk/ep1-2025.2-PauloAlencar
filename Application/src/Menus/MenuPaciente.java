e71cd2070aa43d6768761d5f7911da01f54444ad
Paulodemczuk <paulodemczuk@gmail.com>
2025-09-29 14:36:51 -0300

    Prototipo de menu paciente adicionado

--- Application/src/Menus/MenuPaciente.java
+++ Application/src/Menus/MenuPaciente.java
@@ -1,5 +1,15 @@
 package Menus;
 
 public class MenuPaciente {
-
+    static public void exibirMenuPaciente(){
+        System.out.println("-----Menu Paciente------");
+        System.out.println("1. Exibir Paciente");
+        System.out.println("2. Agendar Consulta");
+        System.out.println("3. Alterar Consulta");
+        System.out.println("4. Historico de Consultas");
+        System.out.println("5. Quarto da internação");
+        System.out.println("6. Historico de Internações");
+        System.out.println(". Adicionar Plano de Saude");
+        System.out.println(". Agendar Consulta para dependentes");
+    }
 }
