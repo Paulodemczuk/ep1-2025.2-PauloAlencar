e8ea306c357b70774e8a266969910642880a9b76
Paulodemczuk <paulodemczuk@gmail.com>
2025-09-29 15:21:52 -0300

    Adicionado prototipo menu cadastro

---
+++ Application/src/Menus/MenuCadastro.java
@@ -0,0 +1,12 @@
+package Menus;
+
+public class MenuCadastro {
+    static public void exibirMenuCadastro(){
+            System.out.println("----- Menu Cadastro ------");
+            System.out.println("1. Cadastrar Paciente");
+            System.out.println("2. Cadastrar Médico");
+            System.out.println("3. Cadastrar Plano de Saude");
+            System.out.println(". ");
+            System.out.println(". ");
+        }
+}
