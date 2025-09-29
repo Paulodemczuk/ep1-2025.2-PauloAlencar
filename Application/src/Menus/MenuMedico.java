e92cfc036b0b7ee774e074b9fc0adfe1428d9c43
Paulodemczuk <paulodemczuk@gmail.com>
2025-09-29 14:37:33 -0300

    Prototipo de menu medico

---
+++ Application/src/Menus/MenuMedico.java
@@ -0,0 +1,15 @@
+package Menus;
+
+public class MenuMedico {
+    static public void exibirMenuMedico(){
+        System.out.println("----- Menu Medico ------");
+        System.out.println("1. Exibir Medico");
+        System.out.println("2. Finalizar Consulta");
+        System.out.println("3. Cancelar Consulta");
+        System.out.println("4. Internar Paciente");
+        System.out.println("5. Cancelar Internação");
+        System.out.println("6. Alterar Agenda de Horarios");
+        System.out.println(". ");
+        System.out.println(". ");
+    }
+}
