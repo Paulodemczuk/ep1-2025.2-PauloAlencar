5d56812d2fe7c3d30ca023b6e8ecf8796eee77ee
Paulodemczuk <paulodemczuk@gmail.com>
2025-10-03 21:28:53 -0300

    Adicionado leitura e escrita de horarios da agenda

--- Application/src/Persistencia/MedicosPersistencia.java
+++ Application/src/Persistencia/MedicosPersistencia.java
@@ -25,7 +25,7 @@ public class MedicosPersistencia {
                 escritor.newLine();
                 
                 for(Medico medico : medicos){
-                    String linha = String.format(Locale.US,"%s,%d,%s,%s,%.2f", medico.getNome(),medico.getIdade(),medico.getCrm(),medico.getEspecialidade(),medico.getCustoConsulta());
+                    String linha = String.format(Locale.US,"%s,%d,%s,%s,%.2f,%s,%s,%s,%s,%s,%s,%s", medico.getNome(),medico.getIdade(),medico.getCrm(),medico.getEspecialidade(),medico.getCustoConsulta(),medico.getHorarios(0),medico.getHorarios(1),medico.getHorarios(2),medico.getHorarios(3),medico.getHorarios(4),medico.getHorarios(5),medico.getHorarios(6));
                     escritor.write(linha);
                     escritor.newLine();
                 }
@@ -52,13 +52,20 @@ public class MedicosPersistencia {
             leitor.readLine();
             while((linha = leitor.readLine()) != null){
                 String[] dados = linha.split(",");
-                if(dados.length == 5){
+                if(dados.length == 12){
                     String nome = dados[0];
                     int idade = Integer.parseInt(dados[1]);
                     String crm = dados[2];
                     String especialidade = dados[3];
                     double custoConsulta = Double.parseDouble(dados[4]);
                     Medico medicoLido = new Medico(nome,idade,especialidade,crm,custoConsulta);
+                    medicoLido.adicionarHorarios(0, dados[5]);
+                    medicoLido.adicionarHorarios(1, dados[6]);
+                    medicoLido.adicionarHorarios(2, dados[7]);
+                    medicoLido.adicionarHorarios(3, dados[8]);
+                    medicoLido.adicionarHorarios(4, dados[9]);
+                    medicoLido.adicionarHorarios(5, dados[10]);
+                    medicoLido.adicionarHorarios(6, dados[11]);
                     medicos.add(medicoLido);
                 }
                 else System.out.println("csv medicos formatado incorretamente");
