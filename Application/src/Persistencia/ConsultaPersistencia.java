cf251cfaa6ad2a3ed8057dfeb4b500cfbb36f078
Paulodemczuk <paulodemczuk@gmail.com>
2025-10-04 19:44:18 -0300

    Adicionada Persistencia para consultas

---
+++ Application/src/Persistencia/ConsultaPersistencia.java
@@ -0,0 +1,83 @@
+package Persistencia;
+
+import Entidades.Consulta;
+import Servicos.MedicoServicos;
+import Servicos.PacienteServicos;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class ConsultaPersistencia {
+
+    private final String nomeArquivo = "Application/dados/consultas.csv";
+    private Path caminhoArquivo;
+
+    public void salvarConsultas(ArrayList<Consulta> consultas){
+        try{
+            caminhoArquivo = Paths.get("Application/dados/");
+            Files.createDirectories(caminhoArquivo);
+            try(BufferedWriter escritor = new BufferedWriter(new FileWriter(nomeArquivo))){
+                escritor.write("Local,Status,Paciente-CPF,Medico-CRM,Data-Hora,Diagnostico");
+                escritor.newLine();
+                
+                for(Consulta consulta : consultas){
+                    String linha = String.format("%s,%d,%s,%s,%s,%s", 
+                    consulta.getLocal(),
+                    consulta.getStatus(),
+                    consulta.getPaciente().getCpf(),
+                    consulta.getMedico().getCrm(),
+                    consulta.getDataHoraFormatada(),
+                    consulta.getDiagnostico()
+                    );
+                    escritor.write(linha);
+                    escritor.newLine();
+                }
+            }
+            catch(IOException e){
+                System.out.println("erro ao salvar o arquivo consultas.csv" + e.getMessage());
+            }
+            
+        }
+        catch(IOException ex){
+            System.out.println("erro ao criar pasta dados" + ex.getMessage());
+        }
+
+    }
+
+    public ArrayList<Consulta> carregarConsultas(PacienteServicos ps, MedicoServicos ms){
+        ArrayList<Consulta> consultas = new ArrayList<>();
+        caminhoArquivo = Paths.get(nomeArquivo);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy-HH:mm");
+
+        if(!Files.exists(caminhoArquivo))return consultas;
+        
+        try(BufferedReader leitor = Files.newBufferedReader(caminhoArquivo)) {
+            String linha;
+            leitor.readLine();
+            while((linha = leitor.readLine()) != null){
+                String[] dados = linha.split(",");
+                if(dados.length == 6){
+                    String local = dados[0];
+                    int status = Integer.parseInt(dados[1]);
+                    String cpf = dados[2];
+                    String crm = dados[3];
+                    LocalDateTime datahora = LocalDateTime.parse(dados[4], formatter);
+                    String diagnostico = dados[5];
+                    Consulta consultaLida = new Consulta(ps.getPaciente(cpf),ms.getMedico(crm),local,status,datahora,diagnostico);
+                    consultas.add(consultaLida);
+                }
+                else System.out.println("csv consultas formatado incorretamente");
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo "+ e.getMessage());
+        }
+        return consultas;
+    }
+}
