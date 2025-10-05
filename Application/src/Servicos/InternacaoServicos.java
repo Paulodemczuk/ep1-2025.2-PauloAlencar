f12c48f8564adfcd244fab301ad4d5596e3386b5
Paulodemczuk <paulodemczuk@gmail.com>
2025-10-04 22:56:38 -0300

    Adicionado suporte para idInternacao

--- Application/src/Servicos/InternacaoServicos.java
+++ Application/src/Servicos/InternacaoServicos.java
@@ -16,14 +16,14 @@ public class InternacaoServicos {
         this.internacoes = internacoes;
     }
 
-    public boolean cadastrarInternacao(Paciente paciente, Medico medico, LocalDate dataEntrada, LocalDate dataSaida, int quarto, double custoInternacao){
+    public boolean cadastrarInternacao(int id, Paciente paciente, Medico medico, LocalDate dataEntrada, LocalDate dataSaida, int quarto, double custoInternacao){
         for(Internacao internacao : internacoes){
             if(internacao.getQuarto() == quarto){
                 System.out.println("quato ocupado");
                 return false;
             }
         }
-        Internacao internacao = new Internacao(paciente, medico, dataEntrada, dataSaida, quarto, custoInternacao);
+        Internacao internacao = new Internacao(id, paciente, medico, dataEntrada, dataSaida, quarto, custoInternacao);
         this.internacoes.add(internacao);
         return true;
     }
@@ -48,5 +48,31 @@ public class InternacaoServicos {
         this.internacoes = internacoes;
     }
 
+    public void printInternacoes(){
+        System.out.println();
+        for(Internacao internacao : internacoes){
+            System.out.println(internacao.getIdInternacao());
+            System.out.println(internacao.getMedico().getNome());
+            System.out.println(internacao.getPaciente().getNome());
+            System.out.println(internacao.getQuarto());
+            System.out.println(internacao.getDataEntradaFormatada());
+            System.out.println(internacao.getDataSaidaFormatada());
+            System.out.println(internacao.getDuracaoInternacao());
+            System.out.println(internacao.getCustoInternacao());
+            System.out.println();
+        }
+    }
+
+    public Internacao getInternacao(int id){
+        Internacao retorno = new Internacao();
+        for(Internacao internacao: internacoes){
+            if(internacao.getIdInternacao() == id){
+                retorno = internacao;
+                break;
+            }
+        }
+        return retorno;
+    
+    }
 
 }
