d57e32870667f86b2d6ce04206b1da4d867e6fe9
Paulodemczuk <paulodemczuk@gmail.com>
2025-10-05 12:31:58 -0300

    Adicionados atributos necessarios, metodos renomeados, agendarConsulta criado

--- Application/src/Menus/MenuPaciente.java
+++ Application/src/Menus/MenuPaciente.java
@@ -1,23 +1,35 @@
 package Menus;
 
+import Entidades.Medico;
+import Entidades.Paciente;
+import Servicos.ConsultaServicos;
+import Servicos.MedicoServicos;
 import Servicos.PacienteServicos;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class MenuPaciente {
-    private PacienteServicos pacienteServicos;
+    private final PacienteServicos pacienteServicos;
+    private final ConsultaServicos consultaServicos;
+    private final MedicoServicos medicoServicos;
 
     public MenuPaciente() {
         this.pacienteServicos = new PacienteServicos();
+        this.consultaServicos = new ConsultaServicos();
+        this.medicoServicos = new MedicoServicos();
     }
-    public MenuPaciente(PacienteServicos pacienteServicos){
+    public MenuPaciente(PacienteServicos pacienteServicos,ConsultaServicos consultaServicos,MedicoServicos medicoServicos){
         this.pacienteServicos = pacienteServicos;
+        this.consultaServicos = consultaServicos;
+        this.medicoServicos = medicoServicos;
     }
 
     
 
-    public void exibirMenuPaciente(Scanner sc){
+    public void acessarMenuPaciente(Scanner sc){
         System.out.println("-----Menu Paciente------");
-        System.out.println("1. Exibir Paciente");
+        System.out.println("1. Exibir Paciente");//
         System.out.println("2. Agendar Consulta");
         System.out.println("3. Alterar Consulta");
         System.out.println("4. Historico de Consultas");
@@ -26,17 +38,24 @@ public class MenuPaciente {
         System.out.println(". Adicionar Plano de Saude");
         System.out.println(". Agendar Consulta para dependentes");
 
-        int escolha = 0; 
-        escolha = sc.nextInt();
-        sc.nextLine();
-       
-        switch (escolha){
-            case 1:
-                this.exibirPaciente(sc);
-                break;
-            case 0:
-                System.out.println("Encerrando...");
-                break;
+        int escolha = -1; 
+        while(escolha != 0){
+            escolha = sc.nextInt();
+            sc.nextLine();
+            switch (escolha){
+                case 1:
+                    exibirPaciente(sc);
+                    break;
+                case 2:
+                    agendarConsulta(sc);
+                    break;
+                case 0:
+                    System.out.println("Voltando ao menu principal...");
+                    break;
+                default:
+                    System.out.println("Escolha uma alternativa valida");
+            }
+
         }
     }
 
@@ -53,7 +72,7 @@ public class MenuPaciente {
                 voltar = sc.nextInt();
                 sc.nextLine();
             } 
-            this.exibirMenuPaciente(sc);
+            this.acessarMenuPaciente(sc);
         }
         else{
             System.out.println("\n"+pacienteServicos.getPaciente(cpf).toString());
@@ -64,7 +83,38 @@ public class MenuPaciente {
                 voltar = sc.nextInt();
                 sc.nextLine();
             } 
-            this.exibirMenuPaciente(sc);
+            this.acessarMenuPaciente(sc);
         }
+    }
+
+    public void agendarConsulta(Scanner sc){
+        int id;
+        Paciente paciente;
+        Medico medico;
+        String local;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy-HH:mm");
+        LocalDateTime dataHora;
+
+        System.out.println("\nAgendar consultas selecionado\n");
+        System.out.println("Digite seu CPF: \n");
+        String cpf = sc.nextLine();
+        System.out.println("\nDigite o CRM do medico desejado: ");
+        String crm = sc.nextLine();
+        System.out.println("\nDigite o local desejado para a consulta: ");
+        local = sc.nextLine();
+        System.out.println("\nDigite a data e hora desejada para a consulta (Siga o formato 'dd/MM/yyyy-HH:mm'): ");
+        String dataHoraFormatada = sc.nextLine();
+        dataHora = LocalDateTime.parse(dataHoraFormatada,formatter);
+
+        System.out.println("As informações estão corretas?");
+        System.out.printf("\nNome do paciente: %s\nNome do medico: %s\nLocal da consulta: %s\nData e hora: %s\n", pacienteServicos.getPaciente(cpf).getNome(),medicoServicos.getMedico(crm).getNome(),local,dataHoraFormatada);        
+
+
+
+
+
+        //int retorno = consultaServicos.cadastrarConsulta(id, paciente, medico, local, status, dataHora);
+
+        double precodaConsulta;
     }
 }
