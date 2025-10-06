fa7945c4e8575ea1a507cf13d8240fb696b9cd3c
Paulodemczuk <paulodemczuk@gmail.com>
2025-10-06 11:45:57 -0300

    Metodo exibirMedico criado

--- Application/src/Menus/MenuMedico.java
+++ Application/src/Menus/MenuMedico.java
@@ -1,7 +1,61 @@
 package Menus;
 
+import Servicos.*;
+import java.util.Scanner;
+
 public class MenuMedico {
+
+    private final PacienteServicos pacienteServicos;
+    private final ConsultaServicos consultaServicos;
+    private final MedicoServicos medicoServicos;
+    private final InternacaoServicos internacaoServicos;
+
+    public MenuMedico(HospitalServicos hospitalServicos) {
+        this.pacienteServicos = hospitalServicos.getPacienteServicos();
+        this.consultaServicos = hospitalServicos.getConsultaServicos();
+        this.medicoServicos = hospitalServicos.getMedicoServicos();
+        this.internacaoServicos = hospitalServicos.getInternacaoServicos();
+    }
+    
+    public void acessarMenuMedico(Scanner sc){
+        
+        int escolha = -1; 
+        while(escolha != 0){
+            exibirMenuMedico();
+            escolha = sc.nextInt();
+            sc.nextLine();
+            switch (escolha){
+                case 1:
+                    exibirMedico(sc);
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 0:
+                    System.out.println("Voltando ao menu principal...");
+                    return;
+                default:
+                    System.out.println("Escolha uma alternativa valida");
+            }
+
+        }
+    }
+
+
     static public void exibirMenuMedico(){
+        System.out.println();
         System.out.println("----- Menu Medico ------");
         System.out.println("1. Exibir Medico");
         System.out.println("2. Finalizar Consulta");
@@ -12,4 +66,33 @@ public class MenuMedico {
         System.out.println(". ");
         System.out.println(". ");
     }
+
+    public void exibirMedico(Scanner sc){
+        System.out.println();
+        System.out.println("Digite o CRM do medico desejado:");
+        String crm = sc.nextLine();
+        System.out.println(crm);
+        if(medicoServicos.crmCadastrado(crm)==false){
+            System.out.println("\nCRM não cadastrado.");
+            System.out.println("\nPressione 0 para voltar.");
+            int voltar = 1;
+            while(voltar != 0){
+                voltar = sc.nextInt();
+                sc.nextLine();
+            }
+        }else{
+            medicoServicos.getMedico(crm).printInformacoes();
+            System.out.println();
+            System.out.println("Pressione 1 para exibir a agenda.");
+            System.out.println("Pressione 0 para voltar.");
+            int voltar = -1;
+            while(voltar != 0){
+                voltar = sc.nextInt();
+                if(voltar == 1)medicoServicos.getMedico(crm).exibirAgenda();
+                sc.nextLine();
+            } 
+            exibirMenuMedico();
+
+        }
+    }    
 }
